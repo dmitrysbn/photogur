9a001b0cce34fa74518676966aa7f8e2f2f0f816
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-02 14:43:56 -0500

    Add authorization to user

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
 
   before_action :ensure_logged_in, except: [:show, :index]
   before_action :load_picture, only: [:show, :edit, :update, :destroy]
-  # before_action :ensure_user_owns_picture, only: [:edit, :update, :destroy]
+  before_action :ensure_user_owns_picture, only: [:edit, :update, :destroy]
 
   def index
     @most_recent_pictures = Picture.most_recent_three
@@ -57,6 +57,13 @@ class PicturesController < ApplicationController
 
   def load_picture
     @picture = Picture.find(params[:id])
+  end
+
+  def ensure_user_owns_picture
+    unless current_user == @picture.user
+      flash[:alert] = "Please log in"
+      redirect_to new_sessions_url
+    end
   end
 
 end
